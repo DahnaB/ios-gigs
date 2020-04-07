e99648db203f0579d91c5058f7ca49d7e677a71f
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-07 15:54:34 -0400

    Completed Part 2

---
+++ Gigs/Gigs/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by Dahna on 4/7/20.
+//  Copyright © 2020 Dahna Buenrostro. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
