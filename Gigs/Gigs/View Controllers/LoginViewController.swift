0f039dd1ba1759be89fdeae22ab7974a7d64fc24
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-07 15:49:07 -0400

    A Test

---
+++ Gigs/Gigs/View Controllers/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  Gigs
+//
+//  Created by Dahna on 4/7/20.
+//  Copyright © 2020 Dahna Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+enum LoginType: String {
+    case signUp = "Sign Up"
+    case signIn = "Sign In"
+}
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var signUpLogInSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    var loginType = LoginType.signUp
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 1:
+            loginType = .signIn
+            passwordTextField.textContentType = .password
+        default:
+            loginType = .signUp
+            passwordTextField.textContentType = .newPassword
+        }
+        
+        submitButton.setTitle(loginType.rawValue, for: .normal)
+    }
+    
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        guard let username = usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+        username.isEmpty == false,
+            let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+        password.isEmpty == false
+            else { return }
+        
+        //let user = User(username: username, password: password)
+    }
+    
+}
